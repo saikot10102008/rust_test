d861aed22093e3c0b6654b8f60b0f715455b323e
Sakhwat Hossain Saikot <saikot10102008@gmail.com>
2026-06-05 05:43:52 +0600

    fc1

---
+++ rust_test/target/debug/deps/rust_test-acd10b34c97682bb.d
@@ -0,0 +1,5 @@
+/Users/saikot_tstmc/rust_test/rust_test/target/debug/deps/rust_test-acd10b34c97682bb.d: src/main.rs
+
+/Users/saikot_tstmc/rust_test/rust_test/target/debug/deps/librust_test-acd10b34c97682bb.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust_test/target/debug/rust_test.d
@@ -0,0 +1,1 @@
+/Users/saikot_tstmc/rust/rust_test/target/debug/rust_test: /Users/saikot_tstmc/rust/rust_test/src/main.rs
